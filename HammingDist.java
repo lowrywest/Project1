fb829735cb8b5730b35536cbb59678cab2e19a67
Lowry West <4lowrywest@gmail.com>
2019-02-12 15:24:46 -0600

    Creating BufferReader and Priming Reads

--- HammingDist.java
+++ HammingDist.java
@@ -8,5 +8,15 @@ public class HammingDist
 	private String secondSTID;
 	private ArrayList <String> allSTID= new ArrayList<String>();
 	
+	public void readInfo() throws IOException
+	{
+			int loc=0;
+			String temp="";
+			BufferedReader STIDVals= new BufferedReader(new FileReader(fileName));
+			STIDVals.readLine();
+			STIDVals.readLine();
+			STIDVals.readLine();
+			
+	}
 	
 }
