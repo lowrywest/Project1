0aa4ca116534f9aa6f475e2060317fc56ca8b80e
Lowry West <4lowrywest@gmail.com>
2019-02-12 15:43:14 -0600

    Fixing while loop for reading in STID

--- HammingDist.java
+++ HammingDist.java
@@ -17,7 +17,7 @@ public class HammingDist
 			STIDVals.readLine();
 			STIDVals.readLine();
 			
-			while(loc<7)
+			while(loc<120)
 			{
 				temp=STIDVals.readLine();
 				temp=temp.substring(1, 5);
